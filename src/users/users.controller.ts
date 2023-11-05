a5364bbdf47de1c73ac021df6cac12268a93d563
Vova <bordiian.v@gmail.com>
2023-11-05 20:53:57 +0200

    learn about controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get() // GET /users or /users?role=value
+    findAll(@Query('role') role?: 'INTERN' | 'ENGiNEER' | 'ADMIN') {
+        return []
+    }
+
+    @Get(':id') // GET /user/:id
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+
+    @Post() // POST /users
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(':id') // PATCH /user/:id
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate }
+    }
+
+    @Delete(':id') // GET /user/:id
+    delete(@Param('id') id: string) {
+        return { id }
+    }
+}
